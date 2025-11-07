1b6edc518679e0b091d2dfebf275d30348945351
rabbitmaid <rabbitmaid@proton.me>
2025-11-07 14:51:22 +0100

    Create a lecturer seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RoleSeeder::class);
         $this->call(AdminSeeder::class);
+        $this->call(LecturerSeeder::class);
     }
 }
---
+++ database/seeders/LecturerSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class LecturerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::firstOrCreate(['email' => 'lecturer@example.com'], [
+            'name' => 'Lecturer User',
+            'email' => 'lecturer@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        $user->assignRole('lecturer');
+    }
+}
